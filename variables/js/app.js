dd7926e0416596211fb5e54b2d38dd68763920bc
habiburrahmanriaz <habiburriaz2@gmail.com>
2024-10-13 15:37:09 +0600

    first commit

---
+++ variables/js/app.js
@@ -0,0 +1,40 @@
+// JavaScript Variables 
+let myName = "Habib";
+
+// Array data type
+let friends = ["Habib, Helal, Jubyer, abdullah"];
+
+// opject data type
+let habibur = {
+  nam: "habib",
+  roll: 10,
+  acountNumber: 1465413,
+  phoneNumber: 459885844,
+  myfuntion: function () {
+    "frist class data type"
+  }
+};
+
+let car = {
+  start: function () {
+    console.log("The car is start");
+  },
+  running: function () {
+    console.log("The car is running");
+  },
+  stopCar: function () {
+    console.log("The car is stop");
+  },
+};
+
+car.start(),
+car.running(),
+car.stopCar(),
+
+
+
+
+
+console.log(friends);
+console.log(habibur.phoneNumber);
+console.log(habibur.myfuntion);
