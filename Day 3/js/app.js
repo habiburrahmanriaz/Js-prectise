221eb330ddff38f4f3488407df918110c528a532
habiburrahmanriaz <habiburriaz2@gmail.com>
2024-11-04 22:55:11 +0600

    Day-3 if else

---
+++ Day 3/js/app.js
@@ -0,0 +1,14 @@
+let userAge = 17;
+
+if (userAge == 17) {
+   console.log("You can access the website!");
+} else {
+  console.log("Sorry! You are not allowed!");
+} 
+ 
+
+// if else shortcut
+let userrAge = 18;
+
+let isAccessable = userrAge > 18 ? true : false;
+console.log(isAccessable);
