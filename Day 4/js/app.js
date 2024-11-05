4f839dfafca19f61052dcc1c225fac0b12d2f7bb
habiburrahmanriaz <habiburriaz2@gmail.com>
2024-11-05 23:57:16 +0600

    Switch Statment

---
+++ Day 4/js/app.js
@@ -0,0 +1,15 @@
+// Switch statment
+
+let userAge = 18;
+
+switch (userAge) {
+  case 1:
+    console.log("You are loggin");
+    break;
+  case 2:
+    console.log("Sorry for loggin");
+    break;
+  default:
+    console.log("You are not available");
+    break;
+}
