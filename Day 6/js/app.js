6afde2f71b56702a240e0a40a4d9d8c37e8552fd
habiburrahmanriaz <habiburriaz2@gmail.com>
2024-11-07 00:41:48 +0600

    math function

---
+++ Day 6/js/app.js
@@ -0,0 +1,14 @@
+function sum1(number1, number2) {
+  console.log(number1 + number2);
+}
+sum1(10, 20);
+
+function sum2(number1, number2) {
+  console.log(number1 + number2);
+}
+sum2(40, 60);
+
+function sum3(number1, number2) {
+  console.log(number1 + number2);
+}
+sum3(20, 50);
