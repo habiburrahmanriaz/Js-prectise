608bfd1cbcd53ebe8ea00afd0fd475d4788d8202
habiburrahmanriaz <habiburriaz2@gmail.com>
2024-11-11 00:51:51 +0600

    Dom and Element change

---
+++ Day 8 Dom and Element Change/js/app.js
@@ -0,0 +1,12 @@
+let pTag = document.getElementById("demo");
+// add text
+pTag.innerHTML = "Hello Dom!";
+
+// add class style
+pTag.classList.add('text');
+
+// add style
+pTag.style.color = "red";
+
+// add style withe
+pTag.style.backgroundColor = 'yellow';
