d2079f96d1bb2eb402d01d0ed07c43a09542adf9
habiburrahmanriaz <habiburriaz2@gmail.com>
2024-11-11 23:49:54 +0600

    Day 9

---
+++ Day 9 Dom manupaltion/js/app.js
@@ -0,0 +1,9 @@
+let userName = document.getElementById("username");
+let password = document.getElementById("username");
+let submitBtn = document.getElementById("submit");
+
+submitBtn.addEventListener('click', function () {
+  console.log(userName.value);
+});
+
+
