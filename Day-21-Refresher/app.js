b2e2371867dfd0cec30bcc5be176f0c2f0422b66
habiburrahmanriaz <habiburriaz2@gmail.com>
2024-11-30 00:11:47 +0600

    Refresher

---
+++ Day-21-Refresher/app.js
@@ -0,0 +1,26 @@
+// data types in js
+// String
+// Number
+// Boolean
+// null
+// object
+// array
+// undefined
+
+// let greeting = "Hello Bangladesh!";
+// let name = "Jhon";
+// let age = 18;
+// let phone = null;
+// let isMerried = true;
+// let role;
+
+// let students = ["Jubyaer", "abudullah", "siam", 324234];
+
+let student = {
+  name: "Jhon",
+  phone: "4353453",
+  age: 18,
+};
+
+let testname = "Test var";
+
