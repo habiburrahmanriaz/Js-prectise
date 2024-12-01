2b38128defb41ca0ba2d47408628c3ac9f278e54
habiburrahmanriaz <habiburriaz2@gmail.com>
2024-12-02 00:17:25 +0600

    Refresher2

---
+++ Day-22 Refresher-2/js/app.js
@@ -0,0 +1,16 @@
+let number1 = 11;
+let number2 = 12;
+let isMerrid = true;
+
+if (number1 > number2) {
+  console.log("Hello Javascript");
+} else {
+  console.log("Sorry!");
+}
+
+
+ /* if (isMerrid) {
+   console.log(Hello);
+ }*/
+
+// if (0 false, 1 true);
