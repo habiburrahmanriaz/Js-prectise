0d5403eaa582989698519bde73d02ac30eb4f185
habiburrahmanriaz <habiburriaz2@gmail.com>
2024-12-03 00:19:19 +0600

    Refresher

--- Day-22 Refresher-2/js/app.js
+++ Day-22 Refresher-2/js/app.js
@@ -14,21 +14,58 @@
 
 // if (0 false, 1 true);
 
-// switch 
-let number = 1;
+// switch
+// let number = 1;
 
-switch (number) {
-  case 1:
-    console.log("Number One");
-    break;
-  case 2:
-    console.log("Number Two");
-    break;
-  case 3:
-    console.log("Number Three");
-    break;
+// switch (number) {
+//   case 1:
+//     console.log("Number One");
+//     break;
+//   case 2:
+//     console.log("Number Two");
+//     break;
+//   case 3:
+//     console.log("Number Three");
+//     break;
   
-  default:
-    console.log("No Number");
-    break;
-}
+//   default:
+//     console.log("No Number");
+//     break;
+// }
+
+// let userName = "Hello"
+
+// types of loop
+// for loop
+// foreach loop
+// do while loop
+// for in loop
+// for of loop
+
+// for loop
+// for (let index = 0; index < 10; index++){
+//  console.log(userName);
+// }
+
+// while loop
+// let index = 1;
+
+// while (index < 10) {
+//   console.log("Hello");
+//   index++;
+// }
+
+
+Array
+let dristicts = [
+  "Dhaka",
+  "Cumilla",
+  "Kholna",
+  "Barishal",
+  [1, 2, 3, 4,],
+  { name: "test", phone: 310654 },
+  
+ ];
+
+ console.log(dristicts);
+
