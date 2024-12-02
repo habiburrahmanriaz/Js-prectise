f6cd9d763fe1d8b5fa970107dc45e5226db1d72c
habiburrahmanriaz <habiburriaz2@gmail.com>
2024-12-02 23:49:35 +0600

    Switch

--- Day-22 Refresher-2/js/app.js
+++ Day-22 Refresher-2/js/app.js
@@ -1,16 +1,34 @@
-let number1 = 11;
-let number2 = 12;
-let isMerrid = true;
+// let number1 = 11;
+// let number2 = 12;
+// let isMerrid = true;
 
-if (number1 > number2) {
-  console.log("Hello Javascript");
-} else {
-  console.log("Sorry!");
-}
-
+// if (number1 > number2) {
+//   console.log("Hello Javascript");
+// } else {
+//   console.log("Sorry!");
+// }
 
  /* if (isMerrid) {
    console.log(Hello);
  }*/
 
 // if (0 false, 1 true);
+
+// switch 
+let number = 1;
+
+switch (number) {
+  case 1:
+    console.log("Number One");
+    break;
+  case 2:
+    console.log("Number Two");
+    break;
+  case 3:
+    console.log("Number Three");
+    break;
+  
+  default:
+    console.log("No Number");
+    break;
+}
