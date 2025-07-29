ef430b45aee1501bc1ab5c337e8cdf3b0e0d1042
habiburrahmanriaz <habiburriaz2@gmail.com>
2025-07-29 18:39:24 +0600

    Opject

--- Custom function/js/app.js
+++ Custom function/js/app.js
@@ -1,3 +1,16 @@
 function customFunction() {
   alert("Hello");
 }
+
+
+var car = {
+  Name: "Habib",
+  Number: 14664,
+  Age: 5,
+  AccountName: "Bank",
+  Stard: function () { },
+  Stop: function () { }
+  
+}
+
+console.log(car. Name);
