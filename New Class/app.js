65d119ce1a6e8201c8969d19ec4377a66579d20a
habiburrahmanriaz <habiburriaz2@gmail.com>
2025-08-08 17:14:56 +0600

    Add

--- New Class/app.js
+++ New Class/app.js
@@ -3,13 +3,13 @@ var addbtn = document.getElementById('add-item');
 var addul = document.getElementById('student-list');
 
 
-addbtn.addEventListener('click', function(){
+addbtn.addEventListener('click', function () {
   if (Inputelm.value) {
-    var licreate = document.createElement('li');
-    licreate.textContent = Inputelm.value;
-    addul.appendChild(licreate);
+    var createli = document.createElement("li");
+    createli.textContent = Inputelm.value;
+    addul.appendChild(createli);
     Inputelm.value = "";
   } else {
-    alert("Kicho nai")
+    alert("hgfh")
   }
-});
+})
